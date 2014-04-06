bbeb22c788fde58d5d17c96343c42c5c8edb9a34
Alexandra Swart <answart@sbcglobal.net>
2014-04-06 00:30:53 -0700

    * Fixed Bcrypt to work with forms

--- app/helpers/sessions.rb
+++ app/helpers/sessions.rb
@@ -1,7 +1,9 @@
 helpers do
-
-  def current_user
-    # TODO: return the current user if there is a user signed in.
+  def loggedin?
+    !!session[:user_id]
   end
 
+  def current_user
+    @logged_in_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ActiveRecord::Base
-  #TODO : Use bcrypt to store hashed passwords and authenticate users
+
+  include BCrypt
+
+  def password
+    password ||= Password.new(self.password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user.password == params[:password]
+      @user
+    else
+      return nil
+    end
+  end
 end
+
+
+
