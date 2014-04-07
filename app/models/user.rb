304e2cc083c21dd2b5c11783a7ce76f624bf5484
Alexandra Swart <answart@sbcglobal.net>
2014-04-07 02:02:56 -0700

    * Update Bcrypt routes/model methods to adhere to gem's updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,25 @@
 class User < ActiveRecord::Base
 
-  include BCrypt
+  # From old version of Bcrypt. Doesnt work
+  # include BCrypt
 
-  def password
-    password ||= Password.new(self.password_hash)
-  end
+  # def password
+  #   password ||= Password.new(self.password_hash)
+  # end
 
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
+  # def password=(new_password)
+  #   @password = Password.create(new_password)
+  #   self.password_hash = @password
+  # end
 
-  def login
-    @user = User.find_by_email(params[:email])
-    if @user.password == params[:password]
-      @user
-    else
-      return nil
-    end
-  end
+  # def login
+  #   @user = User.find_by_email(params[:email])
+  #   if @user.password == params[:password]
+  #     @user
+  #   else
+  #     return nil
+  #   end
+  # end
 end
 
 
